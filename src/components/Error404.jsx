a9fbc42ac2e767ca8097e00aa433075187854947
Lan Dam <ldam77@yahoo.com>
2018-09-07 16:21:25 -0700

    edit error404 page to show props

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Error404() {
+function Error404(props) {
   return (
     <div>
-      <h1>Error 404: Resource not found!</h1>
+      <h1>The page {props.location.pathname} does not exist!</h1>
+      <h3>Would you like to return <Link to='/'>Home</Link>?</h3>
     </div>
   );
 }
