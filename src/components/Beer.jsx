90177010fb69b1f9fca75ebb97a52c5981a1e085
Lan Dam <ldam77@yahoo.com>
2018-09-07 10:55:30 -0700

    add basic framework for BeerList and Beer

---
+++ src/components/Beer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Beer(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+    </div>
+  );
+}
+
+Beer.propTypes = {
+  name: PropTypes.string.isRequired,
+  brewer: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  abv: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  remaining: PropTypes.number.isRequired,
+}
+
+export default Beer;
