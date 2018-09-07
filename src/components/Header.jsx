4212c01eac61d4edb65332601bcdc08d8a013868
Lan Dam <ldam77@yahoo.com>
2018-09-07 15:47:16 -0700

    edit some styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,16 +6,15 @@ function Header() {
     <div>
       <style jsx>{`
         .header {
-          font-size: 100px;
           text-align: center;
-          color: #e2dad5;
+          color: #6b5404;
         }
         .link {
           text-align: right;
         }
         `}</style>
       <div className="header">
-        <h1>Tap Room</h1>
+        <h1>Tap Room Brewery</h1>
       </div>
       <div className="link">
         <Link to="/">Home</Link> | <Link to="/newbeer">Add New Beer</Link>
